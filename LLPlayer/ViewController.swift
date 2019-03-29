0147d4f0130ce3f2bdcb86510fb2c44e0960129c
lizhenliang <lizhenliang@bytedance.com>
2019-03-29 15:02:46 +0800

    [feat] repo init

---
+++ LLPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LLPlayer
+//
+//  Created by Leppard on 2019/3/29.
+//  Copyright © 2019 Leppard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
