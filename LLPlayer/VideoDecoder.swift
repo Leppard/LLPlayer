54ba112ab2323dd44f74ce5d63f3354e9d916558
lizhenliang <lizhenliang@bytedance.com>
2019-03-31 14:32:31 +0800

    [feat] ToolBox to convert data from "Annex B" to "AVCC" and back

--- LLPlayer/VideoDecoder.swift
+++ LLPlayer/VideoDecoder.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import VideoToolbox
+
+struct NALUPacket {
+    var size: Int
+    var data: [UInt8]
+}
+
+class VideoDecoder {
+    private var sampleBuffer: CMSampleBuffer?
+    
+    public func decode(fromData data: Data) -> CMSampleBuffer? {
+        // 将NALU的startCode替换成AVCC的lengthCode
+        let avcc = ToolBox.annexToAvcc(data: data)
+        guard let a = avcc else {
+            return nil
+        }
+        
+        let naluType: UInt8 = a[4] & 0x1F
+        switch naluType {
+        case 5:
+            // I Frame
+            print("I Frame")
+        case 7:
+            // SPS
+            print("SPS")
+        case 8:
+            // PPS
+            print("PPS")
+            
+        default:
+            // Include P/B Frame
+            print("Default NALU")
+        }
+        
+        return nil
+    }
+}
---
+++ LLPlayer/VideoPlayer.swift
@@ -0,0 +1,9 @@
+//
+//  VideoPlayer.swift
+//  LLPlayer
+//
+//  Created by Leppard on 2019/3/29.
+//  Copyright © 2019 Leppard. All rights reserved.
+//
+
+import Foundation
